79b1eea3add7593c9c3f67ac674cea93abc7d848
Ismael <isma.sanfer.94@gmail.com>
2016-03-20 22:44:21 +0100

    Cambio controlador por defecto

--- application/views/principal.php
+++ application/views/principal.php
@@ -3,6 +3,7 @@
 print_r($cartelera);
 ?>
 <br>_________<br>
+CARTELERA:<br>
 
 <?php foreach ($cartelera as $pelicula):?>
 <a href="<?=base_url()."pelicula?id=".$pelicula["id"]?>"><?=$pelicula["titulo"]?></a><br>
