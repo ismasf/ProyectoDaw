0f49fa334a1bed7d33fc2e1f194524e70e7fc58c
Carlos Garbajosa Barroso <Carlos Garbajosa Barroso>
2016-03-23 10:23:18 +0100

    funcionando email de verificacion(PHPMailer)

---
+++ application/views/registro/verificado.php
@@ -0,0 +1,13 @@
+
+<h1>Hola</h1>
+
+
+<?php if ($status==0):?>
+
+<h1>Hemos tenido un problema al verificar su usuario.</h1>
+
+<?php else:?>
+
+<h1>Su correo ha sido verificado correctamente.</h1>
+
+<?php endif;?>
