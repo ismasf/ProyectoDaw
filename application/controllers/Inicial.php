dfd2d132ae01bd630a263068ec2445d83201904e
AmaurySanchez <reimy316@hotmail.com>
2016-03-26 13:25:41 +0100

    Primera version a compartir

---
+++ application/controllers/Inicial.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once ('application/libraries/rb.php');
+class Inicial extends CI_Controller{
+	
+	
+	public function index(){
+		R::setup('mysql:host=localhost;dbname=proyecto', 'root', '');
+		$this->load->model ( 'cartelera_model');
+		
+		$data["cartelera"]= $this->cartelera_model->getCartelera();
+		
+		R::close();
+		$this->template->load("falso","inicio",$data);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+?>
---
+++ application/views/inicio.php
@@ -0,0 +1,6 @@
+<br>_________<br>
+CARTELERA:<br>
+
+<?php foreach ($cartelera as $pelicula):?>
+<a href="<?=base_url()."pelicula?id=".$pelicula["id"]?>"><?=$pelicula["titulo"]?></a><br>
+<?php endforeach;?>
