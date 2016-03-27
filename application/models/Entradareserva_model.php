b715985be4b2aef1427c4cbe43a3a4c6d7b3b49a
Ismael <isma.sanfer.94@gmail.com>
2016-03-28 01:39:27 +0200

    Cambios en la select

--- application/models/Entradareserva_model.php
+++ application/models/Entradareserva_model.php
@@ -3,7 +3,7 @@ require_once ('application/libraries/rb.php');
 class Entradareserva_model extends CI_Model{
 	
 	public function getAsientosReservadosSesion($idSesion){
-		$asiento = R::getAll("SELECT * FROM entradareserva WHERE sesion_id = $idSesion");
+		$asiento = R::getAll("SELECT * FROM entradareserva WHERE sesion_id = :id", array(':id'=>$idSesion));
 		
 		return $asiento;
 	
