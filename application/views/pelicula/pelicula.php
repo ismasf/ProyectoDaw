5a0658d24e03d3dc190c008a00037391ce77374d
AmaurySanchez <reimy316@hotmail.com>
2016-03-29 01:16:01 +0200

    Peliculas semi hecha

--- application/views/pelicula/pelicula.php
+++ application/views/pelicula/pelicula.php
@@ -1,39 +1,52 @@
-<?php 
-	print_r($pelicula);
-	print_r($sesiones);
-?>
-<br>__________________________<br>
+<!--  /*<?php
+print_r ( $pelicula );
+print_r ( $sesiones );
+?>*/
+--> 
+<br>
+__________________________
+<br>
 
 
 Pagina de Pelicula
-
-<h1><?=$pelicula->titulo?></h1>
-<img width="150" height="200" src="<?= base_url()?>assets/img/<?=$pelicula->id?>.png">
-
-Director: <?= $pelicula->director?>
-<br>
-<?php 
-
-$data = file_get_contents(base_url()."assets/json/peliculas.json");
-$json = json_decode($data, true);
-//print_r($products);
-echo $json["peliculas"]["id".$pelicula->id]["Sinopsis"];
-
-/*$json["peliculas"]["id3"]["Sinopsis"]="Pruebaaaaa";
-
-$fh = fopen("assets/json/peliculas.json", 'w');
-fwrite($fh, json_encode($json,JSON_UNESCAPED_UNICODE));
-fclose($fh);
-*/
-
-?>
-<br>
+<div class="container-fluid">
+	<h1><?=$pelicula->titulo?></h1>
+	<div class="col-md-10">
+		<div class="media-left">
+			<img width="150" height="200"
+				src="<?= base_url()?>assets/img/<?=$pelicula->id?>.png">
+		</div>
+		<div class="media-body">
+			
+			Director: <?= $pelicula->director?>
+			<br>
+			<?php
+			
+			$data = file_get_contents ( base_url () . "assets/json/peliculas.json" );
+			$json = json_decode ( $data, true );
+			// print_r($products);
+			echo $json ["peliculas"] ["id" . $pelicula->id] ["Sinopsis"];
+			
+			/*
+			 * $json["peliculas"]["id3"]["Sinopsis"]="Pruebaaaaa";
+			 *
+			 * $fh = fopen("assets/json/peliculas.json", 'w');
+			 * fwrite($fh, json_encode($json,JSON_UNESCAPED_UNICODE));
+			 * fclose($fh);
+			 */
+			
+			?>
+			<br>
+		</div>
+	</div>
+</div>
 ______________________
 <br>
 Tiene estas sesiones:
 <br>
 <?php foreach ($sesiones as $sesion):?>
-<a href="<?=base_url()?>sesion?idSesion=<?=$sesion["id"]?>"><?= substr($sesion["hora"], 11)?></a><br>
+<a href="<?=base_url()?>sesion?idSesion=<?=$sesion["id"]?>"><?= substr($sesion["hora"], 11)?></a>
+<br>
 <?php endforeach;?>
 
 
