e4262010fba7a6d0166e4ad1f01e5ce4e56008f7
Ismael <isma.sanfer.94@gmail.com>
2016-03-29 02:24:12 +0200

    Modificacion de los asientos

--- application/views/sesion/asientos.php
+++ application/views/sesion/asientos.php
@@ -4,23 +4,26 @@
 <?php
 $cont = 0;
 foreach ( $asientos as $asiento ) {
-	$color = "green";
+	$color = "verde";
 	foreach ( $reservas as $reserva ) {
 		if (in_array ( $asiento ["id"], $reserva )) {
-			$color = "red";
+			$color = "rojo";
 		}
 	}
 	foreach ( $bloqueados as $bloqueado ) {
 		if (in_array ( $asiento ["id"], $bloqueado )) {
-			$color = "yellow";
+			$color = "azul";
 		}
 	}
-	if ($cont % 10 == 0) {
+	if ($cont % 21 == 0) {
 		
-		echo "</tr><tr><td bgcolor=$color id=\"" . $asiento ["id"] . "\" ".($color=="red"?"":($color=="yellow"?"":"onclick=asiento(".$asiento ["id"].")")).">" . $asiento ["nom"] . "</td>";
+		//echo "</tr><tr><td bgcolor=$color id=\"" . $asiento ["id"] . "\" ".($color=="red"?"":($color=="yellow"?"onclick=asientoDesbloquear(".$asiento ["id"].")":"onclick=asiento(".$asiento ["id"].")")).">" . $asiento ["nom"] . "</td>";
+		echo "<br><img width=35 height=35 src=\"".base_url()."assets/img/asientos/".$color.".png\" id=\"".$asiento["id"]."\" ".($color=="rojo"?"":($color=="azul"?"onclick=asientoDesbloquear(".$asiento ["id"].")":"onclick=asiento(".$asiento ["id"].")")).">";
 	} else {
-		echo "<td bgcolor=$color id=\"" . $asiento ["id"] . "\" ".($color=="red"?"":($color=="yellow"?"onclick=asientoDesbloquear(".$asiento ["id"].")":"onclick=asiento(".$asiento ["id"].")")).">" . $asiento ["nom"] . "</td>";
+		//echo "<td bgcolor=$color id=\"" . $asiento ["id"] . "\" ".($color=="red"?"":($color=="yellow"?"onclick=asientoDesbloquear(".$asiento ["id"].")":"onclick=asiento(".$asiento ["id"].")")).">" . $asiento ["nom"] . "</td>";
+		echo "<img width=35 height=35 src=\"".base_url()."assets/img/asientos/".$color.".png\" id=\"".$asiento["id"]."\" ".($color=="rojo"?"":($color=="azul"?"onclick=asientoDesbloquear(".$asiento ["id"].")":"onclick=asiento(".$asiento ["id"].")")).">";
 	}
+	
 	$cont ++;
 }
 
