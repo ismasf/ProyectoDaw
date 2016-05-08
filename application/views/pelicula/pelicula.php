42df72e5d612e3df2cc72ff27e792ba0c1a17713
AmaurySanchez <reimy316@hotmail.com>
2016-05-08 17:04:44 +0200

    Aniadido el menu con las sesiones, esta sin formato.

--- application/views/pelicula/pelicula.php
+++ application/views/pelicula/pelicula.php
@@ -39,15 +39,63 @@ ______________________
 <br>
 Tiene estas sesiones:
 <br>
-<?php foreach ($sesiones as $sesion):?>
 
-Dia:<?=substr($sesion["hora"],8,2)?> <a href="<?=base_url()?>sesion?idSesion=<?=$sesion["id"]?>"><?= substr($sesion["hora"], 11)?></a>
-<br>
-<?php endforeach;?>
+<div class="container">
+	<div class="row">
+		                                <div class="col-md-8">
+                                    <!-- Nav tabs --><div class="card">
+                                    <ul class="nav nav-tabs" role="tablist">
+                                        <li role="presentation" class="active"><a href="#lunes" aria-controls="lunes" role="tab" data-toggle="tab">Lunes</a></li>
+                                        <li role="presentation"><a href="#martes" aria-controls="martes" role="tab" data-toggle="tab">Martes</a></li>
+                                        <li role="presentation"><a href="#miercoles" aria-controls="miercoles" role="tab" data-toggle="tab">Miercoles</a></li>
+                                        <li role="presentation"><a href="#jueves" aria-controls="jueves" role="tab" data-toggle="tab">Jueves</a></li>
+                                        <li role="presentation"><a href="#viernes" aria-controls="viernes" role="tab" data-toggle="tab">Viernes</a></li>
+                                        <li role="presentation"><a href="#sabado" aria-controls="sabado" role="tab" data-toggle="tab">Sabado</a></li>
+                                        <li role="presentation"><a href="#domingo" aria-controls="domingo" role="tab" data-toggle="tab">Domingo</a></li>
+                                    </ul>
 
-
-
-
-
-
+                                    <!-- Tab panes -->
+                                    <div class="tab-content">
+                                    	<?php $lun="";$mar="";$mie="";$jue="";$vie="";$sab="";$dom="";?>
+                                        <?php foreach ($sesiones as $sesion):?>
+                                        	<?php $hora=strtotime(substr($sesion["hora"],0,10));
+                                        		if(date("w",$hora)==1){
+                                        			$lun.='<a href="'.base_url().'sesion?idSesion='.$sesion["id"].'">'.substr($sesion["hora"], 11).'</a><br/>';
+                                        		}
+                                        		elseif(date("w",$hora)==2){
+                                        			$mar.='<a href="'.base_url().'sesion?idSesion='.$sesion["id"].'">'.substr($sesion["hora"], 11).'</a><br/>';
+                                        		}
+                                        		
+                                        		elseif(date("w",$hora)==3){
+                                        			$mie.='<a href="'.base_url().'sesion?idSesion='.$sesion["id"].'">'.substr($sesion["hora"], 11).'</a><br/>';
+                                        		}
+                                        		
+                                        		elseif(date("w",$hora)==4){
+                                        			$jue.='<a href="'.base_url().'sesion?idSesion='.$sesion["id"].'">'.substr($sesion["hora"], 11).'</a><br/>';
+                                        		}
+                                        		elseif(date("w",$hora)==5){
+                                        			$vie.='<a href="'.base_url().'sesion?idSesion='.$sesion["id"].'">'.substr($sesion["hora"], 11).'</a><br/>';
+                                        		}
+                                        		elseif(date("w",$hora)==6){
+                                        			$sab.='<a href="'.base_url().'sesion?idSesion='.$sesion["id"].'">'.substr($sesion["hora"], 11).'</a><br/>';
+                                        		}
+                                        		elseif(date("w",$hora)==0){
+                                        			$dom.='<a href="'.base_url().'sesion?idSesion='.$sesion["id"].'">'.substr($sesion["hora"], 11).'</a><br/>';
+                                        		}
+                                        	
+                                        	?>
+                                        
+                                        <?php endforeach;?>
+                                        <div role="tabpanel" class="tab-pane active" id="lunes"><?=$lun ?></div>
+                                        <div role="tabpanel" class="tab-pane" id="martes"><?=$mar ?></div>
+                                        <div role="tabpanel" class="tab-pane" id="miercoles"><?=$mie ?></div>
+                                        <div role="tabpanel" class="tab-pane" id="jueves"><?=$jue ?></div>
+                                        <div role="tabpanel" class="tab-pane" id="viernes"><?=$vie ?></div>
+                                        <div role="tabpanel" class="tab-pane" id="sabado"><?=$sab ?></div>
+                                        <div role="tabpanel" class="tab-pane" id="domingo"><?=$dom ?></div>
+                                    </div>
+</div>
+                                </div>
+	</div>
+</div>
 
